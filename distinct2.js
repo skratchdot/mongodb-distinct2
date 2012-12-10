f9ab3a7b44e7b6bac8b0ae07de56cd9c17641ef9
skratchdot <git@skratchdot.com>
2012-12-09 22:58:09 -0500

    Printing "status" info every 1000ms.
    Helps user experience when processing large collections.

--- distinct2.js
+++ distinct2.js
@@ -1,57 +1,34 @@
-/*global DBCollection: false, DBQuery: false */
-/*jslint indent: 4, plusplus: true */
+/*global DBCollection, DBQuery, print */
+/*jslint indent: 4, plusplus: true, nomen: true */
 /**
  * MongoDB - distinct2.js
  * 
- *      Version: 1.2
- *         Date: October 7, 2012
+ *      Version: 1.3
+ *         Date: December 9, 2012
  *      Project: http://skratchdot.com/projects/mongodb-distinct2/
  *  Source Code: https://github.com/skratchdot/mongodb-distinct2/
  *       Issues: https://github.com/skratchdot/mongodb-distinct2/issues/
  * Dependencies: MongoDB v1.8+
  *               JSON2.js (https://github.com/douglascrockford/JSON-js)
  * 
- * Description:
- * 
- * Similar to the db.myCollection.distinct() function, distinct2() allows
- * you to pass in an array of keys to get values for.  It also allows you
- * to pass in an optional "count" argument.  When true, you can easily get
- * the counts for your distinct values.
- * 
- * You can also call distinct2() on the results of a query (something you
- * can't currently do with the built in distinct() function).
- * 
- * To accomplish this, it adds the following functions to our built in mongo prototypes:  
- * 
- *     DBCollection.prototype.distinct2 = function (keys, count) {};
- *     DBQuery.prototype.distinct2 = function (keys, count) {};
- *
- * Usage:
- * 
- * // All 5 of these statements are the same as:
- * //
- * //     db.users.distinct('name.first')
- * //
- * db.users.distinct2('name.first');
- * db.users.distinct2('name.first', false);
- * db.users.distinct2(['name.first']);
- * db.users.distinct2(['name.first'], false);
- * db.users.find().distinct2('name.first');
- * 
- * // you can pass in an array of values
- * db.users.distinct2(['name.first','name.last']);
- * 
- * // you can get counts
- * db.users.distinct2('name.first', true);
- * 
- * // you can get distinct values from the results of a query
- * db.users.find({'name.first':'Bob'}).distinct2('name.last');
- * 
+ * Copyright (c) 2012 SKRATCHDOT.COM
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
  */
 (function () {
-    'use strict';
+	'use strict';
 
-    var isArray, getFromKeyString;
+        // config variables
+    var currentDate,
+        currentTick = (new Date()).getTime(),
+        previousTick = (new Date()).getTime(),
+        statusIntervalInMs = 10000,
+        // functions
+        isArray,
+        getFromKeyString,
+        printStatus,
+        setStatusInterval;
 
     /**
      * Same behavior as Array.isArray()
@@ -90,6 +67,40 @@
 
     /**
      * @function
+     * @name printStatus
+     * @private
+     * @param currentDocNum {number} - Number of the current document being processed
+     * @param numDocs {number}       - Total number of documents being processed
+     * @param distinctCount {number} - Total number of distinct items found so far
+     */
+    printStatus = function (currentDocNum, numDocs, distinctCount) {
+        // Output some debugging info if needed
+        if (statusIntervalInMs > 0) {
+            currentDate = new Date();
+            currentTick = currentDate.getTime();
+            if (currentTick - previousTick > statusIntervalInMs) {
+                print('Processed ' + currentDocNum + ' of ' + numDocs + ' document(s) and found ' + distinctCount + ' distinct items at ' + currentDate);
+                previousTick = currentTick;
+            }
+        }
+    };
+
+    /**
+     * @function
+     * @name setStatusInterval
+     * @private
+     * @param intervalInMs {number} Will print out a status message after this many
+     *                              milliseconds. If a non-positive number is passed in,
+     *                              then no status messages will be printed.
+     */
+    setStatusInterval = function (intervalInMs) {
+        if (typeof intervalInMs === 'number') {
+            statusIntervalInMs = intervalInMs;
+        }
+    };
+
+    /**
+     * @function
      * @name distinct2
      * @memberOf DBQuery
      * @param keys {string|array} The array of dot delimited keys to get the distinct values for
@@ -100,6 +111,9 @@
      */
     DBQuery.prototype.distinct2 = function (keys, count) {
         var i = 0,
+            currentDocNum = 0,
+            numDocs = this.size(),
+            distinctCount = 0,
             returnArray = [],
             tempArray = [],
             arrayOfValues = false,
@@ -116,22 +130,32 @@
             return returnArray;
         }
 
+        // update tick for printing status line
+        previousTick = (new Date()).getTime();
+
         // populate data object
         this.forEach(function (obj) {
-            var i, values = [], key = '';
+            var i, values = [], key = '', isDefined = false;
             for (i = 0; i < keys.length; i++) {
                 values[i] = getFromKeyString(obj, keys[i]);
+                if (typeof values[i] !== 'undefined') {
+                    isDefined = true;
+                }
             }
-            key = JSON.stringify(values);
-            if (data.hasOwnProperty(key)) {
-                data[key].count = data[key].count + 1;
-            } else {
-                dataOrder.push(key);
-                data[key] = {
-                    values : values,
-                    count : 1
-                };
+            if (isDefined) {
+                key = JSON.stringify(values);
+                if (data.hasOwnProperty(key)) {
+                    data[key].count = data[key].count + 1;
+                } else {
+                    dataOrder.push(key);
+                    data[key] = {
+                        values : values,
+                        count : 1
+                    };
+                }
             }
+            // print status info
+            printStatus(currentDocNum++, numDocs, dataOrder.length);
         });
 
         // should we return an array of values?
@@ -179,11 +203,14 @@
             }
         }
         if (!excludeId) {
-            fields['_id'] = 0;
+            fields._id = 0;
         }
         return this.find({}, fields).distinct2(keys, count);
     };
 
+    // Attach setStatusInterval to both versions of distinct2
+    DBQuery.prototype.distinct2.setStatusInterval = setStatusInterval;
+    DBCollection.prototype.distinct2.setStatusInterval = setStatusInterval;
 }());
 
 
